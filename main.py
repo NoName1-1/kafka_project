d6dc7e790863096443cf929efb846c4b796c11bc
temir <temirlankazh01032003@icloud.com>
2025-02-09 23:34:46 +0500

    second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import datetime
+from datetime import timedelta
 
 
 response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").text
@@ -10,7 +12,15 @@ print(data)
 
 print('----------------------------')
 
-print(data['time']['updated'], data['bpi']['USD']['rate_float'])
+date_time = datetime.datetime.strptime(data['time']['updated'], '%b %d, %Y %H:%M:%S %Z')
+
+# date_formatted = datetime.datetime.strftime(data_time, '%Y.%m.%d')
+# time_formatted = datetime.datetime.strptime(data_time, '%H:%M:%S').time()
+date_time = date_time + timedelta(hours=5) #Время Астаны
+print(type(date_time))
+count_on_dollars = data['bpi']['USD']['rate_float']
+
+print(date_time, count_on_dollars)
 
 
 
